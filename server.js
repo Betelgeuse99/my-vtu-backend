eb13cc5cce180cb08fc475875c560f736d3a0cdd
Betelgeuse99 <mustafamarte@gmail.com>
2026-08-13 01:57:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+// ✅ ALWAYS LOAD .env FIRST
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
